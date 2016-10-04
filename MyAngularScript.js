0aaa5b0acc9926b4a41159efb2b0b3399214c85e
HeyImRige <Robert.aron.tx@verizon.net>
2016-10-03 20:18:45 -0500

    changed some of the test variables

--- MyAngularScript.js
+++ MyAngularScript.js
@@ -14,8 +14,8 @@ angularApp.controller('mainController',['$scope','$sce','$window', function($sco
     /*TODO: move this data into a json file */ 
     $scope.projects = [
         
-        {"type":"audio","date":1293883200,"title":"[Future Bass] Candy","media":"https://www.youtube.com/embed/xNQH2ZLkUi4","content":"'Candy!' is a future bass track produced in fl studio. Following the general trend of future bass tracks, it contains bits of short synths and samples that give the track a cute tone and sound."},
-        {"type":"audio","date":1293883200,"title":"WOW AUDIO PROJECT","media":"https://www.youtube.com/embed/LAGoi9QgShk","content":"hello this is some information about that audio thing"},
+        {"type":"audio","date":1462597200000 ,"title":"[Future Bass] Candy","media":"https://www.youtube.com/embed/xNQH2ZLkUi4","content":"'Candy!' is a future bass track produced in fl studio. Following the general trend of future bass tracks, it contains bits of short synths and samples that give the track a cute tone and sound."},
+        {"type":"audio","date":1474174800000,"title":"[Drumstep] Overwatch Victory Remix","media":"https://www.youtube.com/embed/LAGoi9QgShk","content":"Overwatch remix yeah yeah yeah"},
         {"type":"software","date":1420113600,"title":"WOW SOFTWARE PROJECT","media":"https://puu.sh/rkrA7/c8d7a3517e.png","content":"hello this is some information about that software thing"},
         {"type":"software","date":1420113600,"title":"WOW SOFTWARE PROJECT","media":"imgur.com","content":"hello this is some information about that software thing"},
         {"type":"software","date":1420113600,"title":"WOW SOFTWARE PROJECT","media":"imgur.com","content":"hello this is some information about that software thing"}
