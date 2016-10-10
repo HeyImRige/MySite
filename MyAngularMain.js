74bd9843fea840697a867841e0bb34e415195402
HeyImRige <Robert.aron.tx@verizon.net>
2016-10-10 18:33:47 -0500

    changes to style that deal with how article content is shown, mostly to move the additional link button to the far corner

--- MyAngularMain.js
+++ MyAngularMain.js
@@ -12,11 +12,11 @@ angularApp.controller('mainController',['$scope','$sce','$window', function($sco
     /*TODO: move this data into a json file */ 
     $scope.projects = [
         
-        {"type":"audio","date":1462597200000 ,"title":"[Future Bass] Candy","media":"https://www.youtube.com/embed/xNQH2ZLkUi4","content":"'Candy!' is a future bass track produced in fl studio. Following the general trend of future bass tracks, it contains bits of short synths and samples that give the track a cute tone and sound."},
+        {"type":"audio","date":1462597200000 ,"title":"[Future Bass] Candy","media":"https://www.youtube.com/embed/xNQH2ZLkUi4","content":"'Candy!' is a future bass track. Following the general trend of future bass tracks, it contains bits of short synths and samples that give the track a cute tone and sound. Many of the elements are samples that are pitched up to produce a chipmunk style, while synths maintain a simple and clean quality that prevents that from becoming too harsh.","contentSmall":"'Candy!' is a future bass track. Following the general trend of future bass tracks, it contains bits of short synths and samples that give the track a cute tone and sound."},
         {"type":"audio","date":1474174800000,"title":"[Drumstep] Overwatch Victory Remix","media":"https://www.youtube.com/embed/LAGoi9QgShk","content":"Overwatch remix yeah yeah yeah"},
         {"type":"audio","date":1473051600000,"title":"[Epic Movie Trailer] An Adversary","media":"https://www.youtube.com/embed/gVGhPOorDr4","content":"Track in the styel movie trailer yeah yeah"},
-         {"type":"audio","date":1471669200000,"title":"[Progressive House] Burning","media":"https://www.youtube.com/embed/SMBXaJPF9sM","content":"Progressive house track yeah yeah promoted and featured by Phantom Stone Records"},
-        {"type":"software","date":1460869200000,"title":"Never Late Smart Alarm","media":"smartAlarm.png","content":"Never late smart alarm is a native android appliacation. Type in your destination and time you need to be there, and it will notify you when you need to leave. Google traffic data is called in real time, so you wont be surprised by wrecks on your commute to work. Sudden snow storms or road closing are no worry either. Sleep easy!","additionalLink":"http://devpost.com/software/neverlate-smartalarm-1vyjbi","contentSmall":"Never late smart alarm is a native android appliacation. It uses real time google traffic data, so you'll never be late due to traffic!","additionalText":"Read more on Devpost"},
+         {"type":"audio","date":1471669200000,"title":"[Progressive House] Burning","media":"https://www.youtube.com/embed/SMBXaJPF9sM","content":"'Burning' is a progressive house track. The track features female vocals that are complimented by a driving rythmic chord progression. Many of the lead synths are short samples of vocal chops, as per the style. 'Burning' is promoted by the indie label 'Phantom Stone Records'.","additionalLink":"https://soundcloud.com/phantomstonerecords/progressive-house-rige-burning-phantom-stone-release","additionalText":"View on Soundcloud"},
+        {"type":"software","date":1460869200000,"title":"Never Late Smart Alarm","media":"smartAlarm.png","content":"Never late smart alarm is a native android application. Type in your destination and time you need to be there, and it will notify you when you need to leave. Google traffic data is called in real time, so you wont be surprised by wrecks on your commute to work. Sudden snow storms or road closing are no worry either. Sleep easy!","additionalLink":"http://devpost.com/software/neverlate-smartalarm-1vyjbi","contentSmall":"Never late smart alarm is a native android application. It uses real time google traffic data, so you'll never be late due to traffic!","additionalText":"Read more on Devpost"},
         {"type":"software","date":1476334800000,"title":"This Site!","media":"websiteGif.gif","content":"This is my site! The project was a way for me to experiement with angular and bootstrap. An additional perk was a way to show off some of the projects I have worked on. It is scaleable, both in size in terms of window size, and additional data being added.","contentSmall":"This is my site! The project was a way for me to experiement with angular and bootstrap. An additional perk was a way to show off some of the projects I have worked on. It is scaleable, both in size in terms of window size, and additional data being added."},
         {"type":"software","date":1420113600,"title":"UTA Now","media":"imgur.com","content":"hello this is some information about that software thing"},
         {"type":"software","date":1420113600,"title":"Mav Schedueling Service","media":"imgur.com","content":"helps make your schedule for school","additionalLink":"https://github.com/GroupOmicron/MSS2","additionalText":"Read more on Github"}
@@ -90,16 +90,16 @@ angularApp.directive('resize', function ($window) {
             scope.style = function () {
                 if(scope.windowWidth>1000){
                     return {
-                        'width': '590px'
+                        'width': '610px'
                     };
                 }else if(scope.windowWidth>700){
                     return {
-                        'width':  Math.floor(scope.windowWidth*(0.6)) + 'px'
+                        'width':  'inherit'
                     };
                 }
                 else{
                     return {
-                        'width': Math.floor(scope.windowWidth*(.9)) + 'px'
+                        'width': 'inherit'
                     };
                 }
             };
